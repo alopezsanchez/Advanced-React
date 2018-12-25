678cf07d17676eccd16f3e7fac7414b83e7c35ed
Alejandro López Sánchez <alopez@corunet.com>
2018-12-26 00:14:15 +0100

    use _app.js to define the page layout

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -3,10 +3,8 @@ import Link from 'next/link';
 const Home = props => (
   <div>
     <p>Hey!</p>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
-  </div>  
+
+  </div>
 )
 
 export default Home;
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,12 +1,7 @@
-import Link from 'next/link';
-
 const Sell = props => (
   <div>
     <p>Sell!</p>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-  </div>  
+  </div>
 )
 
 export default Sell;
